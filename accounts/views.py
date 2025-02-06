f098d620f2fc1bb25abcf6d32630b266b21eee1d
Rajeev <rajeevlakshmanan003@gmail.com>
2025-02-06 20:54:19 +0530

    All layout design completed.

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def sign_up(request):
+    return render(request, 'sign-up.html')
+
+def sign_in(request):
+    return render(request, 'sign-in.html')
--- themes/views.py
+++ themes/views.py
@@ -3,3 +3,13 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def team(request):
+    return render(request, 'team.html')
+
+def contact(request):
+    return render(request, 'contact.html')  
+
