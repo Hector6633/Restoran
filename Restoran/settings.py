cd88694a5d153bfc9f9608e8f57b27df1b3686f5
Rajeev <rajeevlakshmanan003@gmail.com>
2025-02-06 20:59:14 +0530

    Changed database to sqlite3.

--- Restoran/settings.py
+++ Restoran/settings.py
@@ -81,12 +81,8 @@ WSGI_APPLICATION = 'Restoran.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'restoran',
-        'USER': 'root',
-        'PASSWORD': '6633',
-        'HOST':'localhost',
-        'PORT':'3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
