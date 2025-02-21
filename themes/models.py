52273e858f4c4e62e25d560c40e5348094075417
Rajeev <rajeevlakshmanan003@gmail.com>
2025-02-21 17:32:25 +0530

    Implement customer feedback.

--- themes/models.py
+++ themes/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class ContactUs(models.Model):
+    customer_name = models.CharField(max_length=50)
+    customer_email = models.EmailField()
+    customer_subject = models.CharField(max_length=50)
+    customer_message = models.TextField()
+    
+    def __str__(self):
+        return self.customer_name
--- themes/views.py
+++ themes/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from restaurant.models import Menu
+from . models import ContactUs
+from django.contrib import messages
+
 # Create your views here.
 def index(request):
     food_items = {
@@ -11,5 +14,20 @@ def about(request):
     return render(request, 'about.html')
 
 def contact(request):
+    if request.method == 'POST':
+        try:
+            customer_name = request.POST.get('name')
+            customer_email = request.POST.get('email')
+            subject = request.POST.get('subject')
+            message = request.POST.get('message')
+            customer_feedback = ContactUs.objects.create(customer_name=customer_name, customer_email=customer_email, customer_subject=subject, customer_message=message)
+            customer_feedback.save()
+            success_msg = 'Thank you for your feedback'
+            messages.success(request, success_msg)
+            return redirect('contact')
+        except Exception as e:
+            error_msg = 'Something went wrong'
+            messages.error(request, error_msg)
+            return redirect('contact')
     return render(request, 'contact.html')  
 
